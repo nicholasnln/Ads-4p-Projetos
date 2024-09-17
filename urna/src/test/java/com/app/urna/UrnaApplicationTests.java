537f1b91e22069655d6255f64a68f2f84f4e527c
nicholasnln <nicholasnloiola@hotmail.com>
2024-09-16 21:27:00 -0300

    Adicionando nova pasta de arquivo base para projeto urna

---
+++ urna/src/test/java/com/app/urna/UrnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.urna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
